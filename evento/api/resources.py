6a7b95111f421353690c33889066bcf839e6942e
Linguagem de Programação Comercial <lpc.pmw@labin.canoas>
2017-04-17 21:44:26 -0300

    Alterações na API

--- evento/api/resources.py
+++ evento/api/resources.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
-from evento.models import TipoInscricao
+from evento.models import TipoInscricao, Pessoa
 from django.contrib.auth.models import User
+
 
 
 class TipoInscricaoResource(ModelResource):
@@ -13,3 +14,9 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+
+
+class PessoaResource(ModelResource):
+    class Meta:
+        queryset = Pessoa.objects.all()
+        allowed_methods = ['get']
